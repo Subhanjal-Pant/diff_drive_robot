d84a3354fae2718c3a5c0f8eaa6c309106d38683
Subhanjal Pant <subhanjalpant1@gmail.com>
2026-01-10 06:31:15 +0545

    Wheels not moving with odom otherwise okay

---
+++ src/articubot_one/launch/launch_sim.launch.py
@@ -0,0 +1,78 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import LaunchConfiguration
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+import xacro
+
+def generate_launch_description():
+    package_name="articubot_one"
+    set_gz_config = SetEnvironmentVariable('GZ_CONFIG_PATH', '/usr/share/gz')
+    rsp=IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(get_package_share_directory(package_name), 'launch', "rsp.launch.py")]),
+        launch_arguments={'use_sim_time':'true'}.items()
+    )
+    
+    gazebo=IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
+        launch_arguments={'gz_args':'-r empty.sdf'}.items(),
+    )
+    
+    spawn_entity=Node(
+        package='ros_gz_sim', 
+        executable='create', 
+        arguments=['-topic', 'robot_description','-name', 'bot'],output='screen'
+    )
+    
+    # bridge_node=Node(
+    #     package='ros_gz_bridge',
+    #     executable='parameter_bridge',
+    #     arguments=[
+    #         '/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist',
+    #         '/joint_states@sensor_msgs/msg/JointState@gz.msgs.Model',
+    #         '/clock@rosgraph_msgs/msg/Clock@gz.msgs.Clock'
+    #     ],
+    #     output='screen'
+    # )
+    # teleop_node=Node(
+    #     package='teleop_twist_keyboard',
+    #     executable='teleop_twist_keyboard',
+    #     name='teleop',
+    #     prefix='gnome-terminal --',
+    #     output='screen'
+    # )
+    
+
+
+
+    # ros2 run teleop_twist_keyboard teleop_twist_keyboard
+    
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            # GZ -> ROS: These topics flow FROM Gazebo TO ROS
+            '/odom@nav_msgs/msg/Odometry[gz.msgs.Odometry',
+            '/model/bot/tf@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V',            '/clock@rosgraph_msgs/msg/Clock[gz.msgs.Clock',
+            '/joint_states@sensor_msgs/msg/JointState[gz.msgs.Model',
+            # ROS -> GZ: Movement commands flow FROM ROS TO Gazebo
+            '/cmd_vel@geometry_msgs/msg/Twist]gz.msgs.Twist'
+        ],
+        remappings=[
+            ('/model/bot/tf', '/tf'),
+        ],
+        output='screen'
+    )
+    
+    
+    return LaunchDescription([
+        set_gz_config,
+        rsp,
+        gazebo,
+        spawn_entity,
+        bridge,
+        
+    ])
---
+++ src/articubot_one/launch/rviz_view.launch.py
@@ -0,0 +1,56 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import LaunchConfiguration
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+import xacro
+
+def generate_launch_description():
+    use_sim_time=LaunchConfiguration('use_sim_time')
+    pkg_path=get_package_share_directory('articubot_one')
+    xacro_file=os.path.join(pkg_path, 'description', 'robot.urdf.xacro')
+    robot_description_config= xacro.process_file(xacro_file)
+    rviz_config_path=os.path.join(pkg_path, 'config', 'view_bot.rviz')
+    
+    params={'robot_description': robot_description_config.toxml(), 'use_sim_time':use_sim_time}
+    
+    robot_state_publisher=Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[params],
+    )
+    robot_state_publisher_gui=Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        output='screen',
+        parameters=[{'use_sim_time':use_sim_time}],
+    )
+    rviz_node=Node(
+        package='rviz2',
+        executable='rviz2',
+        output='screen',
+        arguments=['-d', rviz_config_path],
+        parameters=[params],
+    )
+    bridge = Node(
+    package='ros_gz_bridge',
+    executable='parameter_bridge',
+    arguments=['/odom@nav_msgs/msg/Odometry[gz.msgs.Odometry',
+               '/tf@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V'],
+    output='screen'
+    )
+    return LaunchDescription([
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description="Use sim time if true"
+        ),
+        
+        robot_state_publisher,
+        robot_state_publisher_gui,
+        rviz_node,
+        bridge,
+        
+    ])
