215643011376498efd90330c12d81b0e16b9de18
Subhanjal Pant <subhanjalpant1@gmail.com>
2026-01-22 20:30:24 +0545

    Working with lidar

--- src/articubot_one/launch/launch_sim.launch.py
+++ src/articubot_one/launch/launch_sim.launch.py
@@ -70,6 +70,7 @@ def generate_launch_description():
             '/joint_states@sensor_msgs/msg/JointState[gz.msgs.Model',
             # ROS -> GZ: Movement commands flow FROM ROS TO Gazebo
             '/cmd_vel@geometry_msgs/msg/Twist]gz.msgs.Twist',
+            '/scan@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan',
         ],
         remappings=[
             ('/model/bot/tf', '/tf'),
